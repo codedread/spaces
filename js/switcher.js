cfcba2c3b81a37c561436d18c13cc42d53bf5045
Jeff Schiller <codedread@gmail.com>
2025-08-30 10:00:23 -0700

    Fix indent in switcher.js

--- js/switcher.js
+++ js/switcher.js
@@ -6,81 +6,81 @@ function getSelectedSpace() {
     return document.querySelector('.space.selected');
 }
 
-    function handleSwitchAction(selectedSpaceEl) {
-        chrome.runtime.sendMessage({
-            action: 'switchToSpace',
-            sessionId: selectedSpaceEl.getAttribute('data-sessionId'),
-            windowId: selectedSpaceEl.getAttribute('data-windowId'),
-        });
+function handleSwitchAction(selectedSpaceEl) {
+    chrome.runtime.sendMessage({
+        action: 'switchToSpace',
+        sessionId: selectedSpaceEl.getAttribute('data-sessionId'),
+        windowId: selectedSpaceEl.getAttribute('data-windowId'),
+    });
+}
+
+function handleCloseAction() {
+    chrome.runtime.sendMessage({
+        action: 'requestClose',
+    });
+}
+
+async function getSwitchKeycodes(callback) {
+    const commands = await chrome.commands.getAll();
+    const commandStr = commands.switchCode;
+    const keyStrArray = commandStr.split('+');
+
+    // get keyStr of primary modifier
+    const primaryModifier = keyStrArray[0];
+
+    // get keyStr of secondary modifier
+    const secondaryModifier =
+        keyStrArray.length === 3 ? keyStrArray[1] : false;
+
+    // get keycode of main key (last in array)
+    const curStr = keyStrArray[keyStrArray.length - 1];
+    let mainKeyCode;
+
+    // TODO: There's others. Period. Up Arrow etc.
+    if (curStr === 'Space') {
+        mainKeyCode = 32;
+    } else {
+        mainKeyCode = curStr.toUpperCase().charCodeAt();
     }
 
-    function handleCloseAction() {
-        chrome.runtime.sendMessage({
-            action: 'requestClose',
-        });
-    }
+    callback({
+        primaryModifier,
+        secondaryModifier,
+        mainKeyCode,
+    });
+}
 
-    async function getSwitchKeycodes(callback) {
-            const commands = await chrome.commands.getAll();
-            const commandStr = commands.switchCode;
-            const keyStrArray = commandStr.split('+');
-
-            // get keyStr of primary modifier
-            const primaryModifier = keyStrArray[0];
-
-            // get keyStr of secondary modifier
-            const secondaryModifier =
-                keyStrArray.length === 3 ? keyStrArray[1] : false;
-
-            // get keycode of main key (last in array)
-            const curStr = keyStrArray[keyStrArray.length - 1];
-            let mainKeyCode;
-
-            // TODO: There's others. Period. Up Arrow etc.
-            if (curStr === 'Space') {
-                mainKeyCode = 32;
-            } else {
-                mainKeyCode = curStr.toUpperCase().charCodeAt();
-            }
-
-            callback({
-                primaryModifier,
-                secondaryModifier,
-                mainKeyCode,
-            });
-    }
-
-    function addEventListeners() {
-        document.getElementById('spaceSelectForm').onsubmit = e => {
-            e.preventDefault();
-            handleSwitchAction(getSelectedSpace());
-        };
-
-        const allSpaceEls = document.querySelectorAll('.space');
-        Array.prototype.forEach.call(allSpaceEls, el => {
-            // eslint-disable-next-line no-param-reassign
-            el.onclick = () => {
-                handleSwitchAction(el);
-            };
-        });
-
-        // Here lies some pretty hacky stuff. Yus! Hax!
-        getSwitchKeycodes(() => {
-            const body = document.querySelector('body');
-
-            body.onkeyup = e => {
-                // listen for escape key
-                if (e.keyCode === 27) {
-                    handleCloseAction();
-                }
-            };
-        });
-    }
-
-    window.onload = () => {
-        chrome.runtime.sendMessage({ action: 'requestAllSpaces' }, spaces => {
-            spacesRenderer.initialise(8, true);
-            spacesRenderer.renderSpaces(spaces);
-            addEventListeners();
-        });
+function addEventListeners() {
+    document.getElementById('spaceSelectForm').onsubmit = e => {
+        e.preventDefault();
+        handleSwitchAction(getSelectedSpace());
     };
+
+    const allSpaceEls = document.querySelectorAll('.space');
+    Array.prototype.forEach.call(allSpaceEls, el => {
+        // eslint-disable-next-line no-param-reassign
+        el.onclick = () => {
+            handleSwitchAction(el);
+        };
+    });
+
+    // Here lies some pretty hacky stuff. Yus! Hax!
+    getSwitchKeycodes(() => {
+        const body = document.querySelector('body');
+
+        body.onkeyup = e => {
+            // listen for escape key
+            if (e.keyCode === 27) {
+                handleCloseAction();
+            }
+        };
+    });
+}
+
+window.onload = () => {
+    chrome.runtime.sendMessage({ action: 'requestAllSpaces' }, spaces => {
+        spacesRenderer.initialise(8, true);
+        spacesRenderer.renderSpaces(spaces);
+        addEventListeners();
+    });
+};
